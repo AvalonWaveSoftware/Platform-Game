31d390e973c30e3769d38c3a7480b004b9c158e8
xXKableXx <tristanscott2005@gmail.com>
2026-06-11 21:00:48 -0500

    Revert changes

--- Platform Game/data/campaign.js
+++ Platform Game/data/campaign.js
@@ -1,10 +1,9 @@
-window.GAME_CAMPAIGN = 
-{
-  "currentIndex": 2,
+window.GAME_CAMPAIGN = {
+  "currentIndex": 0,
   "levels": [
     {
       "name": "Level 1",
-      "completed": false,
+      "completed": true,
       "data": {
         "spawn": {
           "x": 96,
@@ -125,7 +124,7 @@ window.GAME_CAMPAIGN =
           },
           {
             "type": "platform",
-            "x": 192,
+            "x": 256,
             "y": 672,
             "width": 64,
             "height": 24
@@ -135,7 +134,7 @@ window.GAME_CAMPAIGN =
     },
     {
       "name": "Level 2",
-      "completed": false,
+      "completed": true,
       "data": {
         "spawn": {
           "x": 96,
@@ -143,142 +142,163 @@ window.GAME_CAMPAIGN =
         },
         "parts": [
           {
-            "type": "platform",
-            "x": 64,
-            "y": 704,
+            "type": "ground",
+            "x": 96,
+            "y": 864,
             "width": 64,
-            "height": 24
+            "height": 64
           },
           {
-            "type": "platform",
-            "x": 128,
-            "y": 704,
+            "type": "ground",
+            "x": 320,
+            "y": 864,
             "width": 64,
-            "height": 24
+            "height": 64
           },
           {
-            "type": "platform",
-            "x": 192,
-            "y": 704,
+            "type": "ground",
+            "x": 512,
+            "y": 800,
             "width": 64,
-            "height": 24
+            "height": 64
           },
           {
-            "type": "platform",
-            "x": 224,
-            "y": 704,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
+            "type": "ground",
             "x": 288,
-            "y": 704,
+            "y": 672,
             "width": 64,
-            "height": 24
+            "height": 64
           },
           {
-            "type": "platform",
-            "x": 384,
-            "y": 704,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 352,
-            "y": 704,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 288,
-            "y": 640,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 416,
-            "y": 576,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 544,
-            "y": 544,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 704,
-            "y": 576,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 800,
-            "y": 640,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 928,
-            "y": 704,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 1056,
+            "type": "ground",
+            "x": 256,
             "y": 832,
             "width": 64,
-            "height": 24
+            "height": 64
           },
           {
-            "type": "platform",
-            "x": 1248,
-            "y": 960,
+            "type": "ground",
+            "x": 128,
+            "y": 864,
             "width": 64,
-            "height": 24
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 96,
+            "y": 768,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 32,
+            "y": 800,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 64,
+            "y": 864,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 416,
+            "y": 768,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 544,
+            "y": 896,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 640,
+            "y": 864,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 480,
+            "y": 832,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 384,
+            "y": 864,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 704,
+            "y": 800,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 800,
+            "y": 736,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 832,
+            "y": 800,
+            "width": 64,
+            "height": 64
           },
           {
             "type": "goal",
-            "x": 1280,
-            "y": 896,
+            "x": 832,
+            "y": 672,
             "width": 32,
             "height": 64
           },
           {
             "type": "coin",
             "x": 544,
-            "y": 512,
+            "y": 736,
             "width": 20,
             "height": 20
           },
           {
             "type": "coin",
-            "x": 832,
-            "y": 608,
+            "x": 352,
+            "y": 832,
             "width": 20,
             "height": 20
           },
           {
             "type": "coin",
-            "x": 960,
-            "y": 672,
+            "x": 160,
+            "y": 800,
             "width": 20,
             "height": 20
           },
           {
             "type": "coin",
-            "x": 288,
-            "y": 608,
+            "x": 416,
+            "y": 704,
+            "width": 20,
+            "height": 20
+          },
+          {
+            "type": "coin",
+            "x": 736,
+            "y": 768,
             "width": 20,
             "height": 20
           }
@@ -287,259 +307,242 @@ window.GAME_CAMPAIGN =
     },
     {
       "name": "Level 3",
-      "completed": false,
+      "completed": true,
       "data": {
         "spawn": {
-          "x": 32,
-          "y": 1504
+          "x": 96,
+          "y": 640
         },
         "parts": [
           {
             "type": "ground",
-            "x": 0,
-            "y": 1568,
+            "x": 32,
+            "y": 896,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 192,
+            "y": 864,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 608,
+            "y": 736,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 576,
+            "y": 864,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 416,
+            "y": 864,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 352,
+            "y": 864,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 320,
+            "y": 896,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 256,
+            "y": 896,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 128,
+            "y": 928,
             "width": 64,
             "height": 64
           },
           {
             "type": "ground",
             "x": 64,
-            "y": 1568,
+            "y": 736,
             "width": 64,
             "height": 64
           },
           {
             "type": "ground",
-            "x": 96,
-            "y": 1568,
-            "width": 64,
-            "height": 64
-          },
-          {
-            "type": "ground",
-            "x": 160,
-            "y": 1568,
-            "width": 64,
-            "height": 64
-          },
-          {
-            "type": "ground",
-            "x": 224,
-            "y": 1568,
-            "width": 64,
-            "height": 64
-          },
-          {
-            "type": "ground",
-            "x": 288,
-            "y": 1568,
-            "width": 64,
-            "height": 64
-          },
-          {
-            "type": "ground",
-            "x": 384,
-            "y": 1568,
+            "x": 672,
+            "y": 832,
             "width": 64,
             "height": 64
           },
           {
             "type": "ground",
             "x": 480,
-            "y": 1568,
+            "y": 768,
             "width": 64,
             "height": 64
           },
           {
             "type": "ground",
-            "x": 512,
-            "y": 1568,
-            "width": 64,
-            "height": 64
-          },
-          {
-            "type": "ground",
-            "x": 416,
-            "y": 1568,
-            "width": 64,
-            "height": 64
-          },
-          {
-            "type": "ground",
-            "x": 352,
-            "y": 1568,
-            "width": 64,
-            "height": 64
-          },
-          {
-            "type": "platform",
-            "x": 128,
-            "y": 1536,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 160,
-            "y": 1504,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
             "x": 192,
-            "y": 1472,
+            "y": 800,
             "width": 64,
-            "height": 24
+            "height": 64
           },
           {
-            "type": "platform",
-            "x": 224,
-            "y": 1440,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
+            "type": "ground",
             "x": 256,
-            "y": 1408,
+            "y": 768,
             "width": 64,
-            "height": 24
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 576,
+            "y": 672,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 768,
+            "y": 896,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "ground",
+            "x": 864,
+            "y": 896,
+            "width": 64,
+            "height": 64
+          },
+          {
+            "type": "goal",
+            "x": 896,
+            "y": 832,
+            "width": 32,
+            "height": 64
+          },
+          {
+            "type": "coin",
+            "x": 416,
+            "y": 832,
+            "width": 20,
+            "height": 20
+          },
+          {
+            "type": "coin",
+            "x": 288,
+            "y": 864,
+            "width": 20,
+            "height": 20
+          },
+          {
+            "type": "coin",
+            "x": 352,
+            "y": 704,
+            "width": 20,
+            "height": 20
+          },
+          {
+            "type": "coin",
+            "x": 448,
+            "y": 640,
+            "width": 20,
+            "height": 20
+          },
+          {
+            "type": "coin",
+            "x": 608,
+            "y": 640,
+            "width": 20,
+            "height": 20
+          },
+          {
+            "type": "coin",
+            "x": 704,
+            "y": 768,
+            "width": 20,
+            "height": 20
+          },
+          {
+            "type": "coin",
+            "x": 800,
+            "y": 864,
+            "width": 20,
+            "height": 20
           },
           {
             "type": "platform",
-            "x": 288,
-            "y": 1376,
+            "x": 448,
+            "y": 800,
             "width": 64,
             "height": 24
           },
           {
             "type": "platform",
             "x": 320,
-            "y": 1344,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 352,
-            "y": 1312,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 384,
-            "y": 1280,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 416,
-            "y": 1248,
+            "y": 768,
             "width": 64,
             "height": 24
           },
           {
             "type": "platform",
             "x": 480,
-            "y": 1216,
+            "y": 704,
             "width": 64,
             "height": 24
           },
           {
             "type": "platform",
-            "x": 576,
-            "y": 1184,
+            "x": 416,
+            "y": 800,
             "width": 64,
             "height": 24
           },
           {
             "type": "platform",
-            "x": 704,
-            "y": 1184,
+            "x": 448,
+            "y": 736,
             "width": 64,
             "height": 24
           },
           {
             "type": "platform",
-            "x": 736,
-            "y": 1312,
+            "x": 160,
+            "y": 800,
             "width": 64,
             "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 832,
-            "y": 1408,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "platform",
-            "x": 832,
-            "y": 1376,
-            "width": 64,
-            "height": 24
-          },
-          {
-            "type": "goal",
-            "x": 864,
-            "y": 1312,
-            "width": 32,
-            "height": 64
-          },
-          {
-            "type": "coin",
-            "x": 576,
-            "y": 1120,
-            "width": 20,
-            "height": 20
-          },
-          {
-            "type": "coin",
-            "x": 352,
-            "y": 1248,
-            "width": 20,
-            "height": 20
-          },
-          {
-            "type": "coin",
-            "x": 256,
-            "y": 1344,
-            "width": 20,
-            "height": 20
-          },
-          {
-            "type": "coin",
-            "x": 224,
-            "y": 1408,
-            "width": 20,
-            "height": 20
-          },
-          {
-            "type": "coin",
-            "x": 736,
-            "y": 1152,
-            "width": 20,
-            "height": 20
-          },
-          {
-            "type": "coin",
-            "x": 768,
-            "y": 1280,
-            "width": 20,
-            "height": 20
-          },
-          {
-            "type": "coin",
-            "x": 832,
-            "y": 1344,
-            "width": 20,
-            "height": 20
           }
         ]
+      }
+    },
+    {
+      "name": "Level 4",
+      "completed": false,
+      "data": {
+        "spawn": {
+          "x": 96,
+          "y": 640
+        },
+        "parts": []
       }
     }
   ]
